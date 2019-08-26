9090175119f15ed82a7270572a7515ce89c44807
Yannri Zenner <zenner88@gmail.com>
2019-08-26 10:47:17 +0700

    bersih-bersih

--- src/app/app.global.ts
+++ src/app/app.global.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AppGlobals {
-    readonly baseAppUrl: string = 'http://localhost:3000/';
-    readonly baseAPIUrl: string = 'http://localhost:3000/api';
+    readonly baseAppUrl: string = 'http://itkpolri.ptcmk.co.id/';
+    readonly baseAPIUrl: string = 'http://itkpolri.ptcmk.co.id/api';
 }
