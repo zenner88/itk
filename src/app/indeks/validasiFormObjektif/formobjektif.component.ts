c51afa854c24c84930b5e11425cb6ee6f6939a8a
Yannri Zenner <zenner88@gmail.com>
2019-10-17 19:31:21 +0700

    validasi

--- src/app/indeks/validasiFormObjektif/formobjektif.component.ts
+++ src/app/indeks/validasiFormObjektif/formobjektif.component.ts
@@ -14,6 +14,7 @@ import { UploadOutput, UploadInput, UploadFile, humanizeBytes, UploaderOptions }
 @Component({ 
     selector: 'ngx-formobjektif', 
     templateUrl: 'formobjektif.component.html',
+    // styleUrls: ['./formobjektif.component.scss'],
     providers: [AppGlobals],
 
  })
@@ -50,19 +51,21 @@ objek: any[]=[];
 objek2: any[]=[];
 jmlIndikator: any;
 jmlDetails: any;
+fileToUpload: any;
+user = "zenner";
 // convenience getters for easy access to form fields
 
 get f() { return this.dynamicForm.controls; }
 get t() { return this.f.tickets as FormArray; }
 
 ngOnInit() {
-this.now = formatDate(new Date(), 'yyyy/MM/dd', 'en');
+this.now = formatDate(new Date(), 'yyyy-MM-dd HH:mm:ss Z', 'en');
 this.dynamicForm = this.formBuilder.group({
 numberOfTickets: ['', Validators.required],
 tickets: new FormArray([])
 });
 
-this.httpClient.get(this._global.baseAPIUrl + '/View_penilaian_satfungs/getDataByPersonalForm?kodeSatker=641017&idSatfung=SU&kodePeriode=1').subscribe(data => {
+this.httpClient.get(this._global.baseAPIUrl + '/View_penilaian_satfungs/getDataByPersonalForm?kodeSatker=640701&idSatfung=SU&kodePeriode=1').subscribe(data => {
 if(data != undefined || data != null)
 {
     this.headers = data;
@@ -74,23 +77,47 @@ if(data != undefined || data != null)
 }, 
 error => { console.log(error) }); 
 
+// this.httpClient.get(this._global.baseAPIUrl + '/View_satkers/getDataByIdTipeSatker?idTipeSatker=R').subscribe(data => {
+//   if(data != undefined || data != null)
+//   {
+//   this.prinsipx = data;
+//   const datas = JSON.stringify(data);
+//   const datax = JSON.parse(datas);
+//   console.log(datas);
+//   console.log(datax);
+//     datax.forEach(xx => {
+//       this.prinsipList.push({value:xx.id,title:xx.prinsip})   
+//       this.prinsipName = xx.title;                
+//     });
+//   }
+//   localStorage.setItem('gridServicecList', JSON.stringify(this.prinsipList));
+//   this.indikators = this.loadTableSettings(); 
+// }, 
+// error => { console.log(error) });  
+
 console.log(this.now);
-this.httpClient.get(this._global.baseAPIUrl + '/View_penilaian_indikator_alls/getDataBypenilaianIdDanJenisDanKIIDanKsat?penilaianId=114&jenis=&kodeSatfung=TSU&kodeIndikatorInduk=').subscribe(indikator => {
+this.httpClient.get(this._global.baseAPIUrl + '/View_penilaian_indikator_alls/getDataBypenilaianIdDanJenisDanKIIDanKsat?penilaianId=862&jenis=P&kodeSatfung=PSU&kodeIndikatorInduk=').subscribe(indikator => {
     const data = JSON.stringify(indikator);
     var datax = JSON.parse(data); 
     // this.objek = this.sources;
     datax.forEach(xx => {
     this.objek.push({
-        kode_indikator_induk:xx.kode_indikator_induk,
-        indikator:xx.indikator,
-        indikator_induk:xx.indikator_induk,
-        satuan:xx.satuan,
-        nilai:xx.nilai,
-        arsip_link:xx.arsip_link,
-        progress:xx.progress,
-        id:xx.id,
-        jenis:xx.Jenis,
-        details:this.objek2
+      kode_indikator_induk:xx.kode_indikator_induk,
+      indikator:xx.indikator,
+      indikator_induk:xx.indikator_induk,
+      satuan:xx.satuan,
+      nilai:xx.nilai,
+      arsip_link:xx.arsip_link,
+      progress:xx.progress,
+      id:xx.id,
+      jenis:xx.jenis,
+      id_progress:xx.id_progress,
+      kode_indikator_satfung:xx.kode_indikator_satfung,
+      kode_indikator:xx.kode_indikator,
+      pilihan_jawaban:xx.pilihan_jawaban,
+      penilaian_id:xx.penilaian_id,
+      id_tipe_indikator:xx.id_tipe_indikator,
+      details:this.objek2,
     })   
     });
 console.log(this.objek.length);
@@ -98,17 +125,26 @@ this.jmlIndikator = this.objek.length;
     // CRETE ISIAN 
     const numberOfTickets = this.jmlIndikator;
     if (this.t.length < numberOfTickets) {
-        for (let i = this.t.length; i < numberOfTickets; i++) {
-            this.t.push(this.formBuilder.group({
-                id: [this.objek[i].id],
-                nilai: [this.objek[i].nilai],
-                indikator: [this.objek[i].indikator],
-                indikator_induk: [this.objek[i].indikator_induk],
-                progress: [this.objek[i].progress],
-                arsip_link: [this.objek[i].arsip_link],
-                satuan: [this.objek[i].satuan],
-                jenis: [this.objek[i].jenis],
-            }));
+      for (let i = this.t.length; i < numberOfTickets; i++) {
+        this.t.push(this.formBuilder.group({
+          id: [this.objek[i].id],
+          penilaian_id: [this.objek[i].penilaian_id],
+          nilai: [this.objek[i].nilai],
+          indikator: [this.objek[i].indikator],
+          indikator_induk: [this.objek[i].indikator_induk],
+          progress: [this.objek[i].progress],
+          arsip_link: [this.objek[i].arsip_link],
+          satuan: [this.objek[i].satuan],
+          jenis: [this.objek[i].jenis],
+          id_progress: [this.objek[i].id_progress],
+          pilihan_jawaban: [this.objek[i].pilihan_jawaban],
+          waktu_ubah: this.now,
+          diubah_oleh: this.user,
+          kode_indikator_satfung: [this.objek[i].kode_indikator_satfung],
+          kode_indikator: [this.objek[i].kode_indikator],
+          id_tipe_indikator: [this.objek[i].id_tipe_indikator],
+          details: [this.objek[i].details],          
+          }));
         }
     } else {
         for (let i = this.t.length; i >= numberOfTickets; i--) {
@@ -121,33 +157,38 @@ error  => {
     this.showToast("warning", "Koneksi bermasalah", error.message);      
 }
 ); 
+console.log("T", this.t);
 
-console.log("GABUNG");
+console.log("OBJEK");
 console.log(this.objek);
-
-this.httpClient.get(this._global.baseAPIUrl + '/View_penilaian_indikator_alls/getDataBypenilaianIdDanJenisDanKIIDanKsat?penilaianId=114&jenis=D&kodeSatfung=TSU&kodeIndikatorInduk=K01').subscribe(indikator => {
-    const data = JSON.stringify(indikator);
-    var datax = JSON.parse(data); 
+this.httpClient.get(this._global.baseAPIUrl + '/View_penilaian_indikator_alls/getDataBypenilaianIdDanJenisDanKIIDanKsat?penilaianId=862&jenis=D&kodeSatfung=PSU&kodeIndikatorInduk=').subscribe(indikator => {
+  const data = JSON.stringify(indikator);
+  var datax = JSON.parse(data); 
 console.log(datax);
-    // this.objek = this.sources;
-    datax.forEach(xx => {
-    this.objek2.push({
-        kode_indikator_induk:xx.kode_indikator_induk,
-        indikator:xx.indikator,
-        satuan:xx.satuan,
-        nilai:xx.nilai,
-        arsip_link:xx.arsip_link,
-        progress:xx.progress,
-        id:xx.id,
-    })   
-    });
+  // this.objek = this.sources;
+  datax.forEach(xx => {
+  this.objek2.push({
+    kode_indikator_induk:xx.kode_indikator_induk,
+    indikator:xx.indikator,
+    indikator_induk:xx.indikator_induk,
+    satuan:xx.satuan,
+    nilai:xx.nilai,
+    arsip_link:xx.arsip_link,
+    progress:xx.progress,
+    id:xx.id,
+    jenis:xx.Jenis,
+    id_progress:xx.id_progress,
+    kode_indikator_satfung:xx.kode_indikator_satfung,
+    penilaian_id:xx.penilaian_id,
+  })   
+  });
 this.jmlDetails = this.objek2.length;
 console.log(this.jmlDetails);
-    
+  
 },
 error  => {
-    console.log("Error", error);
-    this.showToast("warning", "Koneksi bermasalah", error.message);      
+  console.log("Error", error);
+  this.showToast("warning", "Koneksi bermasalah", error.message);      
 }
 ); 
 console.log("Details");
@@ -156,116 +197,89 @@ console.log(this.t.value.name);
 }
 
 onSubmit() {
-    console.log("WORK!");
-    console.log(this.dynamicForm.value);
-    this.submitted = true;
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.dynamicForm.value));
-}
+  console.log("WORK!");
+  console.log(this.dynamicForm.value.tickets);
+  // save 
+  let jml = this.dynamicForm.value.tickets.length;
+  console.log(this.dynamicForm.value.tickets.length);
 
-onReset() {
-    // reset whole form back to initial state
-    this.submitted = false;
-    this.dynamicForm.reset();
-    this.t.clear();
-}
-
-onClear() {
-    // clear errors and reset ticket fields
-    this.submitted = false;
-    this.t.reset();
-}
-openWindow(contentTemplate) {
-    this.windowService.open(
-      contentTemplate,
-      {
-        title: 'Window content from template',
-        context: {
-          text: 'some text to pass into template',
-        },
+  for (let i = 0; i < jml; i++){
+    let jenis = this.dynamicForm.value.tickets[i].jenis;
+    let id = this.dynamicForm.value.tickets[i].id;
+    let data = this.dynamicForm.value.tickets[i];
+    if (jenis == "P"){
+      // console.log(this.dynamicForm.value.tickets[i].jenis);
+      // console.log(this.dynamicForm.value.tickets[i].id);
+      // console.log("END P");
+      this.httpClient.put(this._global.baseAPIUrl + '/Itk_trn_penilaian_indikators/'+id,data).subscribe(data  => {
+        console.log("PUT Request is successful ", data);
+        this.showToast("success", "Data Tersimpan", id);
       },
-    );
-  }
-// UPLOAD
-onUploadOutput(output: UploadOutput): void {
-    switch (output.type) {
-      case 'allAddedToQueue':
-        //   uncomment this if you want to auto upload files when added
-          const event: UploadInput = {
-            type: 'uploadAll',
-            url: 'http://localhost:3000/api/ContainerPenilaianIndi/files/upload',
-            // url: this._global.baseAppUrl+'src/assets/data',
-            method: 'POST',
-            data: { foo: 'bar' }
-          };
-          this.uploadInput.emit(event);
-        break;
-      case 'addedToQueue':
-        if (typeof output.file !== 'undefined') {
-          this.files.push(output.file);
-        }
-        break;
-      case 'uploading':
-        if (typeof output.file !== 'undefined') {
-          // update current data in files array for uploading file
-          const index = this.files.findIndex((file) => typeof output.file !== 'undefined' && file.id === output.file.id);
-          this.files[index] = output.file;
-        }
-        break;
-      case 'removed':
-        // remove file from array when removed
-        this.files = this.files.filter((file: UploadFile) => file !== output.file);
-        break;
-      case 'dragOver':
-        this.dragOver = true;
-        break;
-      case 'dragOut':
-      case 'drop':
-        this.dragOver = false;
-        break;
-      case 'done':
-        // The file is downloaded
-        break;
+      error  => {
+        console.log("Error", error);
+        this.showToast("warning", "Input / koneksi bermasalah", error.error.error.message);
+      }
+      );
+    }
+    else if (jenis == "D"){
+      // console.log(this.dynamicForm.value.tickets[i].jenis);
+      // console.log(this.dynamicForm.value.tickets[i].id);
+      this.httpClient.put(this._global.baseAPIUrl + '/Itk_trn_penilaian_details/'+id,data).subscribe(data  => {
+        console.log("PUT Request is successful ", data);
+        this.showToast("success", "Data Tersimpan", id);
+      },
+      error  => {
+        console.log("Error", error);
+        this.showToast("warning", "Input / koneksi bermasalah", error.error.error.message);
+      }
+      );
+    }
+    else{
+      console.log("Ga ada jenisnya");
+      console.log(this.dynamicForm.value.tickets[i].id);
     }
   }
- 
-  startUpload(): void {
-    const event: UploadInput = {
-      type: 'uploadAll',
-      url: 'http://localhost:3000/api/ContainerPenilaianIndi/files/upload',
-      // url: 'https://localhost:4000/src/assets/data/',
-      method: 'POST',
-      data: { foo: 'bar' }
-    };
- 
-    this.uploadInput.emit(event);
-  }
- 
-  cancelUpload(id: string): void {
-    this.uploadInput.emit({ type: 'cancel', id: id });
-  }
- 
-  removeFile(id: string): void {
-    this.uploadInput.emit({ type: 'remove', id: id });
-  }
- 
-  removeAllFiles(): void {
-    this.uploadInput.emit({ type: 'removeAll' });
-  }
-private showToast(type: NbComponentStatus, title: string, body: string) {
-    const config = {
-        status: type,
-        destroyByClick: true,
-        duration: 4000,
-        hasIcon: true,
-        position: NbGlobalPhysicalPosition.TOP_RIGHT,
-        preventDuplicates: false,
-    };
-    const titleContent = title ? `${title}` : '';
+  // alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.dynamicForm.value));
+}
 
-    this.index += 1;
-    this.toastrService.show(
-        body,
-        `${titleContent}`,
-        config);
-    }
+openWindow(contentTemplate) {
+  this.windowService.open(
+    contentTemplate,
+    {
+      title: 'Window content from template',
+      context: {
+        text: 'some text to pass into template',
+      },
+    },
+  );
+}
+// UPLOAD
+postMethod(files: FileList) {
+  this.fileToUpload = files.item(0); 
+  let formData = new FormData(); 
+  formData.append('file', this.fileToUpload, this.fileToUpload.name); 
+  this.httpClient.post(this._global.baseAPIUrl+'/ContainerPenilaianIndi/upload/upload', formData).subscribe((val) => {
+  console.log("val");  
+  let da = JSON.stringify(val)
+  let dat = JSON.parse(da)
+  console.log(dat.result.files.file[0].name)
+  });
+  return false; 
+}
+private showToast(type: NbComponentStatus, title: string, body: string) {
+  const config = {
+    status: type,
+    destroyByClick: true,
+    duration: 4000,
+    hasIcon: true,
+    position: NbGlobalPhysicalPosition.TOP_RIGHT,
+    preventDuplicates: false,
+  };
+  const titleContent = title ? `${title}` : '';
+  this.index += 1;
+  this.toastrService.show(
+    body,
+    `${titleContent}`,
+    config);
+  }
 }
