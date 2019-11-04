ba7b83bc15b1cea436016d5a4b912d247d861898
Syamsudin <si.syamsudin@yahoo.co.id>
2019-11-05 04:29:10 +0700

    rev

--- src/app/landing.component.ts
+++ src/app/landing.component.ts
@@ -75,7 +75,31 @@ export class LandingComponent implements OnInit, AfterViewInit {
   satfungx: any;
   dialogActive: any;
   idKelompok: any;
+  config = {
+    displayKey: "title", //if objects array passed which key to be displayed defaults to description
+    search: true //true/false for the search functionlity defaults to false,
+    // height: 'auto' //height of the list so that if there are more no of items it can show a scroll defaults to auto. With auto height scroll will never appear
+    // placeholder:'Select' // text to be displayed when no item is selected defaults to Select,
+    // customComparator: ()=>{} // a custom function using which user wants to sort the items. default is undefined and Array.sort() will be used in that case,
+    // limitTo: options.length // a number thats limits the no of options displayed in the UI similar to angular's limitTo pipe
+    // moreText: 'more' // text to be displayed whenmore than one items are selected like Option 1 + 5 more
+    // noResultsFound: 'No results found!' // text to be displayed when no items are found while searching
+    // searchPlaceholder:'Search' // label thats displayed in search input,
+    // searchOnKey: 'name' // key on which search should be performed this will be selective search. if undefined this will be extensive search on all keys
+  };
 
+  configPolres = {
+    displayKey: "title", //if objects array passed which key to be displayed defaults to description
+    search: true //true/false for the search functionlity defaults to false,
+    // height: 'auto' //height of the list so that if there are more no of items it can show a scroll defaults to auto. With auto height scroll will never appear
+    // placeholder:'Select' // text to be displayed when no item is selected defaults to Select,
+    // customComparator: ()=>{} // a custom function using which user wants to sort the items. default is undefined and Array.sort() will be used in that case,
+    // limitTo: options.length // a number thats limits the no of options displayed in the UI similar to angular's limitTo pipe
+    // moreText: 'more' // text to be displayed whenmore than one items are selected like Option 1 + 5 more
+    // noResultsFound: 'No results found!' // text to be displayed when no items are found while searching
+    // searchPlaceholder:'Search' // label thats displayed in search input,
+    // searchOnKey: 'name' // key on which search should be performed this will be selective search. if undefined this will be extensive search on all keys
+  };
   @BlockUI() blockUI: NgBlockUI;
 
   @HostListener("window:scroll", ["$event"]) // for window scroll events
@@ -102,18 +126,7 @@ export class LandingComponent implements OnInit, AfterViewInit {
     private httpClient: HttpClient,
     private _global: AppGlobals
   ) {}
-  config = {
-    displayKey:"description", //if objects array passed which key to be displayed defaults to description
-    search:true, //true/false for the search functionlity defaults to false,
-    height: 'auto', //height of the list so that if there are more no of items it can show a scroll defaults to auto. With auto height scroll will never appear
-    placeholder:'Select', // text to be displayed when no item is selected defaults to Select,
-    customComparator: ()=>{}, // a custom function using which user wants to sort the items. default is undefined and Array.sort() will be used in that case,
-    // limitTo: options.length, // a number thats limits the no of options displayed in the UI similar to angular's limitTo pipe
-    moreText: 'more', // text to be displayed whenmore than one items are selected like Option 1 + 5 more
-    noResultsFound: 'No results found!', // text to be displayed when no items are found while searching
-    searchPlaceholder:'Search', // label thats displayed in search input,
-    searchOnKey: 'name' // key on which search should be performed this will be selective search. if undefined this will be extensive search on all keys
-    }
+
   ngOnInit() {
     this.httpClient.get(this._global.baseAPIUrl + "/View_satfungs/").subscribe(
       data => {
@@ -126,7 +139,9 @@ export class LandingComponent implements OnInit, AfterViewInit {
           datax.forEach(xx => {
             this.indikatorSatfungList.push({
               value: xx.id,
-              title: xx.singkatan
+              title: xx.singkatan_satfung,
+              singkatan_satfung: xx.singkatan_satfung,
+              id: xx.id
             });
           });
         }
@@ -176,7 +191,7 @@ export class LandingComponent implements OnInit, AfterViewInit {
     this.idKelompok = id;
     this.satkerx = null;
     this.satkerPolda = null;
-    this.getDropdownPolda();
+    // this.getDropdownPolda();
     console.log(id);
   }
 
@@ -195,7 +210,11 @@ export class LandingComponent implements OnInit, AfterViewInit {
             const datax = JSON.parse(datas);
 
             datax.forEach(xx => {
-              this.satkerListPolda.push({ value: xx.kode, title: xx.satker });
+              this.satkerListPolda.push({
+                value: xx.kode,
+                title: xx.satker,
+                kode: xx.kode
+              });
             });
           }
         },
@@ -222,7 +241,11 @@ export class LandingComponent implements OnInit, AfterViewInit {
             const datas = JSON.stringify(data);
             const datax = JSON.parse(datas);
             datax.forEach(xx => {
-              this.satkerList.push({ value: xx.kode, title: xx.satker });
+              this.satkerList.push({
+                value: xx.kode,
+                title: xx.satker,
+                kode: xx.kode
+              });
             });
           }
         },
@@ -555,7 +578,7 @@ export class LandingComponent implements OnInit, AfterViewInit {
     this.dialogActive = this.dialogService.open(dialogPolres);
   }
   openOperator(dialogOperator) {
-    this.getDropdownPolda();
+    // this.getDropdownPolda();
     this.dialogActive = this.dialogService.open(dialogOperator);
     this.idKelompok = 70;
   }
